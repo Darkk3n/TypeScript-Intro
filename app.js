ba6ddcaf7490818b0b6c5ff0602fa48697536e0d
Gerardo <gerraguilar@gmail.com>
2020-09-01 22:35:51 -0500

    Object type and types inference

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-function add(n1, n2, print, phrase) {
-    if (print) {
-        console.log(phrase + ": " + (n1 + n2));
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 2.9;
-var printResult = true;
-var resultPhrase = 'Result is';
-add(number1, number2, printResult, resultPhrase);
+var person = {
+    name: 'Gerardo',
+    age: 31
+};
+console.log(person.name);
--- app.ts
+++ app.ts
@@ -1,16 +1,10 @@
-function add(n1: number, n2: number, print: boolean, phrase: string) {
-    if (print) {
-        console.log(`${phrase}: ${n1 + n2}`);
-    }
-    else {
-        return n1 + n2;
-    }
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+const person = {
+    name: 'Gerardo',
+    age: 31
+};
 
-}
-
-const number1 = 5;
-const number2 = 2.9;
-const printResult = true;
-const resultPhrase = 'Result is';
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);

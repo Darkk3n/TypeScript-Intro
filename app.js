96cbd674dddecbab38b7c52327032c0e739ad29f
Gerardo <gerraguilar@gmail.com>
2020-09-01 22:41:58 -0500

    Arrays and for loop

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
 var person = {
     name: 'Gerardo',
-    age: 31
+    age: 31,
+    hobbies: ['Sports', 'Cooking']
 };
+var favActivities;
+favActivities = ['Sports'];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
 console.log(person.name);
--- app.ts
+++ app.ts
@@ -4,7 +4,13 @@
 // } = {
 const person = {
     name: 'Gerardo',
-    age: 31
+    age: 31,
+    hobbies: ['Sports', 'Cooking']
 };
 
+let favActivities: string[];
+favActivities = ['Sports'];
 console.log(person.name);
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}

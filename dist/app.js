00123f3adaad5b41eee1c30f709bb941ecebd6fc
Gerardo <gerraguilar@gmail.com>
2020-09-19 23:42:30 -0500

    Class methods

--- dist/app.js
+++ dist/app.js
@@ -4,8 +4,19 @@ class Department {
     constructor(n) {
         this.name = n;
     }
+    //Declare a new class method
+    //Instead of starting with function just put the name of the method (camelCase)
+    //And the parameters needed
+    changeName(newName) {
+        this.name = newName;
+    }
 }
 //Instance. Same instead of var, let is used
 let test = new Department('Foo');
-console.log(test);
+console.log('First Name');
+console.log(test.name);
+//Usage of class method
+test.changeName('New Departement name');
+console.log('Name after calling class method');
+console.log(test.name);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,21 @@ class Department {
     constructor(n: string) {
         this.name = n;
     }
+
+    //Declare a new class method
+    //Instead of starting with function just put the name of the method (camelCase)
+    //And the parameters needed
+    changeName(newName: string) {
+        this.name = newName;
+    }
 }
 
 //Instance. Same instead of var, let is used
 let test = new Department('Foo');
+console.log('First Name');
+console.log(test.name);
 
-console.log(test);
+//Usage of class method
+test.changeName('New Departement name');
+console.log('Name after calling class method');
+console.log(test.name);
